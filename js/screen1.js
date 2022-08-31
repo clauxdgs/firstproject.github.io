5236dc4bd1f8f8097e0fb16e094bed05206cbcbe
clauxdgs <108471007+clauxdgs@users.noreply.github.com>
2022-08-31 11:03:47 -0300

    screen 1 changes

--- js/screen1.js
+++ js/screen1.js
@@ -1,3 +1,14 @@
+//creo una variable usuario que lee el usuario almacenado el localStorage y lo muestra en "CAMPO USUARIO" que es un boton agregado en el html
+let usuario;
+if (localStorage.getItem('Usuario') != null) {
+    let htmlContentToAppend="";
+    //localStorage.getItem("key del elemento que queremos obtener")
+    usuario=localStorage.getItem('Usuario');
+    htmlContentToAppend += ` User:` +" "+ usuario + ``;
+    document.getElementById('campoUsuario').innerHTML=htmlContentToAppend;
+    console.log(usuario);
+}
+
 document.addEventListener("DOMContentLoaded", function(){
     document.getElementById("autos").addEventListener("click", function() {
         localStorage.setItem("catID", 101);
@@ -11,4 +22,5 @@ document.addEventListener("DOMContentLoaded", function(){
         localStorage.setItem("catID", 103);
         window.location = "products.html"
     });
+    
 });
