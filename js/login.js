39cca10da853d874f5c3e690f6626a20f9bc70ec
clauxdgs <108471007+clauxdgs@users.noreply.github.com>
2022-08-31 11:04:43 -0300

    login.js save user in localstorage

--- js/login.js
+++ js/login.js
@@ -2,6 +2,8 @@
 document.getElementById("label_pass").style.display = 'none'; 
 document.getElementById("label_user").style.display = 'none'; 
 
+
+
 /* Esta es mi funcion para pintar los borders de los inputs de rojo cuando falta la clave o el usuario */
 function changeColor(casilla){
     document.getElementById(casilla).style.borderColor="red";
@@ -9,13 +11,21 @@ function changeColor(casilla){
 
 /* Con esta funcion llamamos al DOM, la pagina inicia y extrae los datos del formulario y los valida*/
 document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("formulario").addEventListener('submit', verificarLogin);
+    document.getElementById("formulario").addEventListener('submit', validarFormulario);
   });
+
+//guardo el usuario en localStorage
+  function guardarUsuario(){
+    let user= document.getElementById('usuario').value;
+    console.log('usuario') 
+    //localStorage.setItem("KEY","valor que queremos almacenar")
+    localStorage.setItem('Usuario',user);
+  }
 
 
   /* La funcion validar formulario identifica los inputs de nombre y clave, si estan vacios muestra un error, si estan llenos, envia el formulario y nos redirige a la siguiente ventana */
   
-   function validarFormulario(evento) {
+  function validarFormulario(evento) {
     evento.preventDefault();
     var usuario = document.getElementById('usuario').value;
     var inputNombre= document.getElementById('usuario');
@@ -38,6 +48,7 @@ document.addEventListener("DOMContentLoaded", function() {
       return;
     }
     this.submit();
+    guardarUsuario();
   }
 
 
@@ -45,16 +56,16 @@ document.addEventListener("DOMContentLoaded", function() {
 
  // FUNCION QUE VALIDA EL LOGIN DE GOOGLE Y REDIRIGE A LA PAGINA DE INICIO DEL ECOMMERCE
 
-function handleCredentialResponse(response) {   
-  console.log("Encoded JWT ID token: " + response.credential);
-  //console.log("HOLA CLAU");
-  window.location.href="screen1.html"
- }
-window.onload = function () {
-  google.accounts.id.initialize({
-    client_id: "800017163944-am4b22fd7theglhvbg6asgmsn4hhlm1l.apps.googleusercontent.com",
-    callback: handleCredentialResponse
-  });
-    google.accounts.id.prompt(); // also display the One Tap dialog
-}
+// function handleCredentialResponse(response) {   
+//   console.log("Encoded JWT ID token: " + response.credential);
+//   //console.log("HOLA CLAU");
+//   window.location.href="screen1.html"
+//  }
+// window.onload = function () {
+//   google.accounts.id.initialize({
+//     client_id: "800017163944-am4b22fd7theglhvbg6asgmsn4hhlm1l.apps.googleusercontent.com",
+//     callback: handleCredentialResponse
+//   });
+//     google.accounts.id.prompt(); // also display the One Tap dialog
+// }
   
